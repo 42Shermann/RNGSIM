ab4b4df156611bf7b31c03ed056a3391d311f877
unknown <42Shermann>
2021-08-02 23:33:21 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { DATA } from './data/data';
 
 function App() {
+
+  const [itemClass, setClass] = useState('');
+  const [itemType, setType] = useState('');
+  const [itemStats1, setStats1] = useState('');
+  const [itemStats2, setStats2] = useState('');
+  const [itemSkills, setSkill] = useState('');
+
+  function getRandomNumber(min, arr) {
+    let max = arr.length -1 ;
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+    let arrResult = arr[result];
+    return arrResult;
+  }
+
+  function upStates() {
+    setClass(getRandomNumber(0, DATA.classes));
+    setType(getRandomNumber(0, DATA.types));
+    setStats1(getRandomNumber(0, DATA.subStats));
+    setStats2(getRandomNumber(0, DATA.subStats));
+    setSkill(getRandomNumber(0, DATA.additionalSkill));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+        <h1>TM Gear Crafting Simulator</h1>
+        <button 
+          type="button" 
+          className="btn btn__primary btn__lg"
+          onClick={upStates}>
+        Craft</button>
+        <div className="stack-small">
+          <div className="c-cb">
+              <p>Class</p>
+              {itemClass}
+          </div>
+          <div className="c-cb">
+              <p>Type</p>
+              {itemType}
+          </div>
+          <div className="c-cb">
+              <ul>Substats</ul>
+                <li>{itemStats1}</li>
+                <li>{itemStats2}</li>
+          </div>
+          <div className="c-cb">
+              <p>Additonal Skill</p>
+              {itemSkills}
+          </div>
+        </div>  
+      
     </div>
   );
 }
